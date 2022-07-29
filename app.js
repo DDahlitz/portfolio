f740dbd312c5976b64428f89ff9249a6ebc6acad
Derrick Dahlitz <ddahlitz@iMac-28.lan>
2022-07-28 17:43:58 -0700

    finshed rough draft for html, css, and jquery

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 $(() => {
 
+     //
 
+    /*======================= navbar buttons =======================*/
 
-    /*=============== navbar buttons ===============*/
+$('a').addClass('inactive')
+$('a').first().removeClass('inactive').addClass('active')
+
 const $div0 = $('#div0')
 const $div1 = $('#div1')
 const $div2 = $('#div2')
@@ -10,32 +14,66 @@ const $div3 = $('#div3')
 $('#div1').hide()
 $('#div2').hide()
 $('#div3').hide()
+
+/*=========================================================================*/
+/*======================= navbar on hover functions =======================*/
+/*=========================================================================*/
+            //syntax found on stackoverflow
+            //has an issue with the jquery created classes
+            //if buttin is clicked it retains values of the inactive class while becoming an active class
+
+    // $('.inactive').hover(function(){
+    //     $(this).css('color', 'black');
+    //     $(this).css("background-color",  '#ddd');
+    //     }, function() {
+    //     $(this).css('color', '#f2f2f2');
+    //     $(this).css('background-color', ' #333');
+    // });
+
+/*======================= navbar on click functions =======================*/
+
 const displayAboutMe = () => {
     $div1.hide()
+        $('#resume').removeClass('active').addClass('inactive')
     $div2.hide()
+        $('#projects').removeClass('active').addClass('inactive')
     $div3.hide()
+        $('#contact-me').removeClass('active').addClass('inactive')
     $div0.show()
+        $('#about').addClass('active').removeClass('inactive')
 }
 const displayResume = () => {
     $div2.hide()
+        $('#projects').removeClass('active').addClass('inactive')
     $div3.hide()
+        $('#contact-me').removeClass('active').addClass('inactive')
     $div0.hide()
+        $('#about').removeClass('active').addClass('inactive')
     $div1.show()
+        $('#resume').addClass('active').removeClass('inactive')
 }
 const displayProjects = () => {
     $div3.hide()
+        $('#contact-me').removeClass('active').addClass('inactive')
     $div0.hide()
+        $('#about').removeClass('active').addClass('inactive')
     $div1.hide()
+        $('#resume').removeClass('active').addClass('inactive')
     $div2.show()
+        $('#projects').addClass('active').removeClass('inactive')
 }
 const displayContactMe = () => {
     $div0.hide()
+        $('#about').removeClass('active').addClass('inactive')
     $div1.hide()
+        $('#resume').removeClass('active').addClass('inactive')
     $div2.hide()
+        $('#projects').removeClass('active').addClass('inactive')
     $div3.show()
+        $('#contact-me').addClass('active').removeClass('inactive')
 }
 
-/*=============== resume buttons ===============*/
+/*======================= resume buttons =======================*/
 
 $('#education').hide()
 $('#employment').hide()
@@ -60,7 +98,8 @@ const displayInterests = () => {
     $('#interests').show()
 }
 
-/*=============== Portfolio Carousel ===============*/
+/*======================= Portfolio Carousel =======================*/
+
 $(() => {
     let currentProject = 0
     let numberofProjects = $('.carousel-projects').children().length - 1
@@ -84,12 +123,9 @@ $(() => {
     })
 });
 
+/*======================= Function Calls =======================*/
 
-
-
-
-
-
+// $('.navbar a').on('hover', hoverNavbar)
 $('#about').on('click', displayAboutMe)
 $('#resume').on('click', displayResume)
 $('#projects').on('click', displayProjects)
@@ -97,5 +133,4 @@ $('#contact-me').on('click', displayContactMe)
 $('#ed').on('click', displayEducation)
 $('#emp').on('click', displayEmployment)
 $('#int').on('click', displayInterests)
-
 })
